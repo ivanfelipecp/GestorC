78045b3f3c594f5d7f781c93c002ec97d65467e0
Iván Calvo Pérez <ivanfelipecp@gmail.com>
2017-11-26 00:25:24 -0600

    + Seteo de lo que teníamos

---
+++ GestorC/GestorC/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestorC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string correo, string password)
+        {
+            return Content(correo+"-"+password);
+        }
+    }
+}
