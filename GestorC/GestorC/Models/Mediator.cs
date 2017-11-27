adf3908d9633278666fe6d5bf6ca341af4cf3cbc
Odin2 <alepiojo05@gmail.com>
2017-11-26 23:00:00 -0600

    mediator

---
+++ GestorC/GestorC/Models/Mediator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2Consola.Controlador_central
+{
+    interface Mediator
+    {
+        void agregarUsuario(Usuario_Chat u);
+
+        void enviarMensaje(string mensaje, string usuario);
+    }
+}
---
+++ GestorC/GestorC/Models/Usuario_Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Proyecto2Consola.Controlador_central
+{
+    interface Usuario_Chat
+    {
+        void enviarMensaje(string mensaje);
+
+        void recibirMensaje(string mensaje);
+
+        void setMediator(Mediator m);
+    }
+}
